f2823de22fd360d0b8ff5c6f82bea0d6f17c5163
Ubuntu <ubuntu@ip-172-31-32-145.ec2.internal>
2017-08-10 05:16:35 +0000

    change some scripting stuff

--- transfer_learning/img_to_oil.py
+++ transfer_learning/img_to_oil.py
@@ -1,5 +1,5 @@
 from subprocess import call
 
 def convert(filename):
-    call(["convert", filename, "-paint", "2", "-blur", "0x4", "-paint", "4", filename])
+    call(["convert", filename, "-blur", "0x2", "-paint", "3", filename])
 
